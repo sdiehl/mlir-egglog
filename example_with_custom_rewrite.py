c5c64b62b7484f3846818f9c307b076897bb089e
sdiehl <stephen.m.diehl@gmail.com>
2025-07-17 10:00:56 +0100

    run formatter

--- example_with_custom_rewrite.py
+++ example_with_custom_rewrite.py
@@ -4,9 +4,10 @@ import numpy as np  # noqa: E402
 llvmlite.opaque_pointers_enabled = True
 
 from mlir_egglog import kernel  # noqa: E402
-from mlir_egglog.term_ir import Term, Sin, Cos, Mul, Add 
+from mlir_egglog.term_ir import Term, Sin, Cos, Mul, Add
 from egglog import rewrite, ruleset
 from mlir_egglog.basic_simplify import basic_math
+
 
 # A rewrite rule
 @ruleset
@@ -17,19 +18,20 @@ def trig_double_angle(a: Term):
     mul2 = Mul(cos_a, sin_a)
 
     # sin(a)*cos(a) + cos(a)*sin(a) -> 2 * sin(a)*cos(a)
-    yield rewrite(Add(mul1, mul2)).to(
-        Mul(Term.lit_f32(2.0), mul1)
-    )
+    yield rewrite(Add(mul1, mul2)).to(Mul(Term.lit_f32(2.0), mul1))
+
 
 # Apply the rewrites
-@kernel("float32(float32)", rewrites = (basic_math, trig_double_angle))
+@kernel("float32(float32)", rewrites=(basic_math, trig_double_angle))
 def fn(a):
     return np.sin(a) * np.cos(a) + np.cos(a) * np.sin(a)
+
 
 def ref_fn(a):
     return np.sin(a) * np.cos(a) + np.cos(a) * np.sin(a)
 
-# Observe that the output LLVM IR is optimized. 
+
+# Observe that the output LLVM IR is optimized.
 out = fn(np.array([1.0], dtype=np.float32))
 ref = ref_fn(np.array([1.0], dtype=np.float32))
 print(out)
